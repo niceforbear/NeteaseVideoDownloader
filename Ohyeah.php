50733c8c175f95e6a90297001f5b4eb212be8701
liuyk <liuyk@jiedaibao.com>
2016-06-16 14:09:56 +0800

    fix format

--- Ohyeah.php
+++ Ohyeah.php
@@ -26,61 +26,66 @@ class Ohyeah
 
     private $content = []; # content array, which include two section: its name and download link.
 
-    public function __construct($downloadUrl = ''){
-        try{
-            if(empty($downloadUrl)){
+    public function __construct($downloadUrl = '')
+    {
+        try {
+            if (empty($downloadUrl)) {
                 throw new Exception('The download url can\'t be empty.');
-            }else{
+            } else {
                 include('phpquery-master/phpQuery/phpQuery.php');
                 $this->downloadUrl = $downloadUrl;
             }
-        }catch (Exception $e){
+        } catch (Exception $e) {
             print $e->getMessage();
             exit;
         }
     }
 
-    public function finish(){
+    public function finish()
+    {
         phpQuery::newDocumentFileHTML($this->downloadUrl, $this->charset);
         $nodeCount = pq("#list2")->count();
         $articleList = pq("#list2 tr");
 
         echo "Start\n";
-        for($i = 0; $i < $this->loop; $i++)
+        for ($i = 0; $i < $this->loop; $i++) {
             echo '=';
+        }
         echo "\n";
 
         $i = 0;
-        foreach($articleList as $tr){
+        foreach ($articleList as $tr) {
             $i++;
-            if($i <= 1 ){ # Filter the first node which has no meaning.
+            if ($i <= 1) { # Filter the first node which has no meaning.
                 continue;
             }
             $className = $tr->nodeValue; # This class order and its name.
             $classNameCombine = explode(' ', $className);
             $j = 0;
             $this->content[$i-1]['chapterName'] = '';
-            foreach($classNameCombine as $k => $v) { # Put all class order and its name into $this->content array.
+            foreach ($classNameCombine as $k => $v) { # Put all class order and its name into $this->content array.
                 $v = trim($v);
                 if (empty($v)) {
                     continue;
                 }
                 $j++;
-                if($j == 1){
+                if ($j == 1) {
                     $this->content[$i-1]['chapterId'] = $v;
-                }else{
+                } else {
                     $this->content[$i-1]['chapterName'] .= $v . " ";
                 }
             }
             $child = pq("td", $tr)->html();
             $trueContent = explode(' ', $child);
             $linkString = '';
-            foreach($trueContent as $k => $v){
+            foreach ($trueContent as $k => $v) {
                 $v = trim($v);
 
-                if(empty($v)) continue;
+                if (empty($v)) {
+                    continue;
+                }
 
-                if($v == "<a"){
+                if ($v == "<a") {
                     $linkString = $v . ' ' . $trueContent[$k+1];
                     break;
                 }
@@ -94,27 +99,28 @@ class Ohyeah
 
         var_dump($this->content);
 
-        for($i = 0; $i < $this->loop; $i++)
+        for ($i = 0; $i < $this->loop; $i++) {
             echo '=';
+        }
         echo "\nFinished! \n";
     }
 
-    private function curl($item){
-
+    private function curl($item)
+    {
     }
 }
 
 /**
  * Demo usage
  */
-if(!empty($argv)){
-    if($argv[1] == Ohyeah::DECIDE_CODE){
+if (!empty($argv)) {
+    if ($argv[1] == Ohyeah::DECIDE_CODE) {
         $url = empty($argv[2]) ? 'http://open.163.com/special/opencourse/algorithms.html': $argv[2];
         $oy = new Ohyeah($url);
         $oy->finish();
-    }else{
+    } else {
         echo "\nwrong\n";
     }
-}else{
+} else {
     echo "\nYou should type arg 1023 to get demo output. ^_^\n";
 }
